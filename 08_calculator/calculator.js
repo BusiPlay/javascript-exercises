b940d8cb01ca395a0be11f56b288ff29b01cbfb6
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-14 16:13:09 -0400

    Ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (i=0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let prod = 1
+  for (i=0; i < arr.length; i++){
+    prod *= arr[i]
+  }
+  return prod;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(a) {
+  let prod = 1
+  for (i = a; i > 0; i--)  {
+    prod *= i
+  }
+  return prod
 	
 };
 
