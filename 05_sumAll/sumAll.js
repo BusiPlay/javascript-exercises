829c4d886dd221300992deaf87e265893941280e
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-14 15:49:56 -0400

    Ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    if ( start < 0 || end < 0 ) {
+        return "ERROR"
+    }
+    else if (typeof(start) != 'number' || typeof(end) != 'number') {
+        return "ERROR"
+    }
+    else {
+        let sum = 0
+        //allow starting with larger number first and swap
+        if (start > end) {
+            [start, end] = [end, start]
+        }
+        for (let i = start; i <= end; i++) {
+            sum += i
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
