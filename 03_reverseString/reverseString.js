7656518fc4663ff26fe78eef8582cf11215986f5
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-14 14:22:27 -0400

    Ex03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revStr = ''
+    for (i=str.length; i>=0; i--){
+        revStr = revStr.concat(str.charAt(i))
+    }
+    return revStr
 };
 
 // Do not edit below this line
