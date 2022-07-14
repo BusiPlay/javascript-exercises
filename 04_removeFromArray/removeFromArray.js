40e73657ee504e745ffcada42fcefdf8bcc458f4
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-14 15:33:19 -0400

    Ex04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    let srcArray = arguments[0];
+    let trgArray = [];
+    for (let i=1; i<arguments.length; i++){
+        while (srcArray.indexOf(arguments[i]) != -1) {
+            srcArray[srcArray.indexOf(arguments[i])]='';
+        } 
+    }
+    trgArray = srcArray.filter(n=>n);
+    return trgArray;
 
 };
 
