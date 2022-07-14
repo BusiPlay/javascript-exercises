7dd37372250918901bc017d6198197bd3d29a53b
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-14 15:54:34 -0400

    Ex06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(testYear) {
+    if (testYear%400 == 0) {
+        return true
+    }
+    else if (testYear % 100 == 0) {
+        return false
+    }
+    else if (testYear % 4 == 0) {
+        return true
+    }
+    else return false
 
 };
 
