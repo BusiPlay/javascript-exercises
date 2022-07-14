e2359e66f4bf7e190491c1215b56fca66f02bbd0
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-14 13:51:41 -0400

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    else return string.repeat(count);
 };
 
 // Do not edit below this line
