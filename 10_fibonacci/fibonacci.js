7e40c3f8a47e5214aa75e0b3ecd4eda93cd8ed05
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-15 05:46:35 -0400

    Ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(seq) {
+    //calculates n'th number if fibonacci sequence
+    if (seq < 0 ) {
+        return "OOPS"
+    }
+    arr = [0,1]
+    for (i=2;  i <= seq; i++) {
+        arr[i]=arr[i-2] + arr[i-1]
+    }
+    console.log(arr)
+    return arr[seq]
 };
 
 // Do not edit below this line
