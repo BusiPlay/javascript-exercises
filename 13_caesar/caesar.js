d26402bc1181253866fafc7423fe278fa33d9a99
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-15 08:39:12 -0400

    Ex13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -4,11 +4,18 @@ const caesar = function(str, shift) {
 };
 
 const shiftIfAlpha = function(strChar, shift) {
-    let re = new RegExp("^([a-zA-Z])");
-    if (re.test(strChar)) {
-        console.log(strChar) 
-        return strChar.charCodeAt(0) + shift
-    } else return strChar.charCodeAt(0) 
+    let charCode = strChar.charCodeAt(0)
+    if (shift < 0) {
+        shift = shift % 26 + 26
+    }
+    if (charCode >=65 && charCode < 91) {
+        return ((charCode - 65 + shift) % 26 + 65) 
+    }
+    else if (charCode >= 97 && charCode < 123) {
+        return ((charCode - 97 + shift) % 26 + 97)
+    }
+    else return charCode
+
 }
 // Do not edit below this line
 module.exports = caesar;
