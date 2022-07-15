7ca6a2043a36f3d21e7ecba8efca1c9174f42aaa
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-15 05:57:40 -0400

    Ex11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(lib) {
+    titleArr = []
+    for (i=0; i < lib.length; i++) {
+        titleArr[i] = lib[i]["title"]
+        console.log(lib[i])
+        console.log(typeof(lib[i]))
+    }
+    return titleArr
 
 };
 
