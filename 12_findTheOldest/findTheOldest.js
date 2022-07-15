4d59258e9d3b329bc708bfffcdfb29d8f8b4627f
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-15 06:09:22 -0400

    Ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge=0
+    let oldestPerson = {}
+    for (i=0; i<people.length; i++) {
+        person = people[i]
+        if (!person.yearOfDeath) {
+            today = new Date()
+            person.yearOfDeath = today.getFullYear()
+        }
+        if (person.yearOfDeath - person.yearOfBirth > maxAge) {
+            oldestPerson = person
+            maxAge = person.yearOfDeath - person.yearOfBirth
+        }
+    }
+    return oldestPerson
 };
 
 // Do not edit below this line
