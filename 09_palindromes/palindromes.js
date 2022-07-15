8b0d0101c902b7e852ff6bb33237ad550ec9082e
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-15 05:35:36 -0400

    Ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (maybePalindrome) {
+    maybePalindrome = maybePalindrome.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_ `~()]/g,"").split('')
+    reversed = [...maybePalindrome]
+    reversed.reverse()
+    console.log(reversed)
+    console.log(maybePalindrome)
+    return JSON.stringify(maybePalindrome.filter(n=>n)) == JSON.stringify(reversed.filter(n=>n))
 };
 
 // Do not edit below this line
