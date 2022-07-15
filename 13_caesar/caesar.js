e03164b0197528caee605cfa0aaeaea1d14f0907
Businger <pbusinger@us-c02fp72tml85.lan>
2022-07-15 06:40:10 -0400

    works but no wrapping

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,14 @@
-const caesar = function() {
-
+const caesar = function(str, shift) {
+    return str.split('').map((char)=>
+         shiftIfAlpha(char,shift)).map((a)=>String.fromCharCode(a)).join('')
 };
 
+const shiftIfAlpha = function(strChar, shift) {
+    let re = new RegExp("^([a-zA-Z])");
+    if (re.test(strChar)) {
+        console.log(strChar) 
+        return strChar.charCodeAt(0) + shift
+    } else return strChar.charCodeAt(0) 
+}
 // Do not edit below this line
 module.exports = caesar;
